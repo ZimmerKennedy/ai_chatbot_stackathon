76af63c2c765f42d876ddfbb64cf1530442a82b4
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-01-20 20:50:07 -0900

    server setup

---
+++ client/script.js
@@ -0,0 +1,82 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
+// loader(element) - This function takes in an element as an argument and sets the textContent of that element to an empty string. 
+// sets an interval to add a period to the element's text content every 300ms. If the text content becomes 4 periods long, it will reset to an empty string.
+const loader = (element) => {
+  element.textContent = "";
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+    if (element.textContent === '....'){
+        element.textContent = '';
+    }
+  }, 300);
+};
+// typeText(element, text) - This function takes in an element and a text as arguments.
+// It creates an interval that types out the text passed as an argument to the element passed as an argument, one character at a time, at a rate of 20ms per character.
+const typeText = (element,text) => {
+    let index = 0;
+    let interval = setInterval(() => {
+        if(index < text.length){
+            element.innerHTML += text.charAt(index);
+            index++;
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+// generateUniqueId() - This function creates a unique id by combining the current timestamp, a random number, and a hexadecimal string representation of that random number.
+// It returns the combined string in the format "id-timestamp-hexadecimalString".
+const generateUniqueId = () =>{
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+    return `id-${timestamp}-${hexadecimalString}`
+}
+// create an HTML string that represents a chat message stripe with a profile picture, message text, and unique ID, depending on whether the message is from the AI or the user.
+const chatStripe = (isAi, value, uniqueId) => {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat"> 
+                <div classname="profile">
+                    <img 
+                        src="${isAi ? bot : user}"
+                        alt="${isAi ? 'bot' : 'user'}"
+                    />
+                </div>
+                    <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+        `
+    )
+}
+
+const handleSubmit = async(e) =>{
+    e.preventDefault();
+    
+    const data = new FormData(form);
+        // users chatStripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+    form.reset();
+
+        // bots chatstripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+        // everytime a chat make it view height
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+    const messageDiv = document.getElementById(uniqueId);
+    loader(messageDiv);
+}
+// handles submit
+form.addEventListener('submit', handleSubmit);
+// submit using the enter key
+form.addEventListener('keyup',(e) =>{
+    if(e.keycode === 13){
+        handleSubmit(e)
+    }
+})
---
+++ server/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req,res) =>{
+    res.status(200).send({
+        message: 'Hey its Arti the Chatbot'
+    })
+});
+
+app.post('/', async(req,res) =>{
+    try{
+        const prompt = req.body.prompt;
+        
+        // this model is from https://beta.openai.com/playground/p/default-openai-api?model=text-davinci-003
+        
+        const response = await openai.createCompletion({
+            model:"text-davinci-003",
+            // contains data for the prompt
+            prompt:`${prompt}`,
+            // higher temperature value means the model will take more risk
+            temperature:0,
+            // max tokens to generate || responses
+            max_tokens:3000,
+            top_p:1,
+            // repeating of sentences 
+            frequency_penalty:0.5,
+            presence_penalty:0,
+        });
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch(err){
+        console.log(err)
+        res.status(500).send({err})
+    }
+})
+
+app.listen(8080, () => console.log('Server is running on port http://localhost:8080'))
