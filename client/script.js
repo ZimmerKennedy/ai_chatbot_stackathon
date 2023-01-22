4ae92c0e92d6523aca0ea1c3bf9fc7665b370f84
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-01-22 00:05:43 -0900

    server tweak

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async(e) =>{
     loader(messageDiv);
 
     // fetch data from server => bot's response
-    const response = await fetch('http://localhost:6060',{
+    const response = await fetch('https://arti-the-chatbot.onrender.com/',{
         method: 'POST',
         headers:{
             'Content-Type': 'application/json'
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
-This Chatbot AI
+Arti the Chatbot ai
     uses Vite, Javascript, OpenAI API, node express
+
+    Arti the Chatbot is deployed using 
+    backend: render
+    frontend: vercel
